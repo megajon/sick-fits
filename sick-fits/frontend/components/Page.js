f815f924a403a733dac1a151fb9855032cc6b2e3
Jonathan Seubert <jonathan.seubert@megajon.com>
2021-03-30 18:52:44 -0700

    first commit

---
+++ sick-fits/frontend/components/Page.js
@@ -0,0 +1,11 @@
+import Header from "./Header"
+
+export default function Page({ children }) {
+  return  (
+    <div>
+      <Header />
+      <h2>I am the page component</h2>
+      { children }
+    </div>
+  )
+}
